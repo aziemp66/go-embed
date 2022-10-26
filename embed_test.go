2f255f474a461865cac9e58bbd5c0a707ca9caca
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-26 14:12:23 +0700

    Embed Multiple Files

--- embed_test.go
+++ embed_test.go
@@ -1,6 +1,7 @@
 package goembed
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -22,5 +23,21 @@ func TestBytes(t *testing.T) {
 	err := ioutil.WriteFile("logo_new.jpg", logo, fs.ModePerm)
 	if err != nil {
 		panic(err)
+	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	var filesArray []string = []string{
+		"a", "b", "c",
+	}
+
+	for _, v := range filesArray {
+		x, _ := files.ReadFile(fmt.Sprintf("files/%s.txt", v))
+		fmt.Println(string(x))
 	}
 }
